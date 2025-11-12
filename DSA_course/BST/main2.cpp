a6c83d3530a08d8777fd498759d6ee9a20d19fc5
ahmed18gaa <ahmed18ga@gmail.com>
2025-11-12 18:03:44 +0300

    Refactor BinaryTree class to use int for node values and update traversal outputs in main function

--- DSA_course/BST/main2.cpp
+++ DSA_course/BST/main2.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 class BinaryTree
 {
 public:
-    string value;
+    int value;
     BinaryTree *left_child;
     BinaryTree *right_child;
 
     // Constructor
-    BinaryTree(const string &val)
+    BinaryTree(int val)
     {
         value = val;
         left_child = nullptr;
@@ -19,7 +18,7 @@ public:
     }
 
     // insert_left method
-    void insert_left(const string &val)
+    void insert_left(int val)
     {
         if (left_child == nullptr)
         {
@@ -34,7 +33,7 @@ public:
     }
 
     // insert_right method
-    void insert_right(const string &val)
+    void insert_right(int val)
     {
         if (right_child == nullptr)
         {
@@ -47,6 +46,7 @@ public:
             right_child = new_node;
         }
     }
+
     // pre_order traversal
     void pre_order()
     {
@@ -58,6 +58,7 @@ public:
         if (right_child != nullptr)
             right_child->pre_order();
     }
+
     // in_order traversal
     void in_order()
     {
@@ -69,6 +70,7 @@ public:
         if (right_child != nullptr)
             right_child->in_order();
     }
+
     // post_order traversal
     void post_order()
     {
@@ -84,44 +86,36 @@ public:
 
 int main()
 {
-    BinaryTree *a_node = new BinaryTree("a");
-    a_node->insert_left("b");
-    a_node->insert_right("c");
+    BinaryTree *a_node = new BinaryTree(1);
+    a_node->insert_left(2);
+    a_node->insert_right(3);
 
     BinaryTree *b_node = a_node->left_child;
-    b_node->insert_right("d");
+    b_node->insert_right(4);
 
     BinaryTree *c_node = a_node->right_child;
-    c_node->insert_left("e");
-    c_node->insert_right("f");
+    c_node->insert_left(5);
+    c_node->insert_right(6);
 
-    BinaryTree *d_node = b_node->right_child;
-    BinaryTree *e_node = c_node->left_child;
-    BinaryTree *f_node = c_node->right_child;
-
-    // cout << a_node->value << endl; // a
-    // cout << b_node->value << endl; // b
-    // cout << c_node->value << endl; // c
-    // cout << d_node->value << endl; // d
-    // cout << e_node->value << endl; // e
-    // cout << f_node->value << endl; // f
-
-    cout << "Pre-order traversal:" << endl;
-    a_node->pre_order(); // a, b, d, c, e, f
-
-    cout << "\nIn-order traversal:" << endl;
-    a_node->in_order(); // b, d, a, e, c, f
-
-    cout << "\nPost-order traversal:" << endl;
-    a_node->post_order(); // d, b, e, f, c, a
+    cout << "Pre-order traversal: \n";
+    a_node->pre_order(); // 1 2 4 3 5 6
     cout << endl;
 
-    delete f_node;
-    delete e_node;
-    delete d_node;
-    delete c_node;
-    delete b_node;
-    delete a_node;
+    cout << "In-order traversal: \n";
+    a_node->in_order(); // 2 4 1 5 3 6
+    cout << endl;
+
+    cout << "Post-order traversal: \n";
+    a_node->post_order(); // 4 2 5 6 3 1
+    cout << endl;
+
+    // Free memory (optional cleanup)
+    delete a_node->right_child->right_child; // 6
+    delete a_node->right_child->left_child;  // 5
+    delete a_node->right_child;              // 3
+    delete a_node->left_child->right_child;  // 4
+    delete a_node->left_child;               // 2
+    delete a_node;                           // 1
 
     return 0;
 }
