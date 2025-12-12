6fcae774914fd247462ee5730d51c164b0211ad1
ahmed18gaa <ahmed18ga@gmail.com>
2025-12-12 16:03:24 +0300

    Implement Heap class with insert and print functionality

---
+++ DSA_course/Heaps/main1.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Heap
+{
+private:
+    vector<int> heap;
+
+    int leftChild(int index)
+    {
+        return 2 * index + 1;
+    }
+
+    int rightChild(int index)
+    {
+        return 2 * index + 2;
+    }
+
+    int parent(int index)
+    {
+        return (index - 1) / 2;
+    }
+
+    void swap(int index1, int index2)
+    {
+        int temp = heap[index1];
+        heap[index1] = heap[index2];
+        heap[index2] = temp;
+    }
+
+public:
+    void printHeap()
+    {
+        cout << "\n[";
+        for (size_t i = 0; i < heap.size(); i++)
+        {
+            cout << heap[i];
+            if (i < heap.size() - 1)
+            {
+                cout << ", ";
+            }
+        }
+        cout << "]" << endl;
+    }
+
+    const vector<int> &getHeap() const
+    {
+        return heap;
+    }
+
+    void insert(int value)
+    {
+        heap.push_back(value);
+        int current = heap.size() - 1;
+
+        while (current > 0 && heap[current] > heap[parent(current)])
+        {
+            swap(current, parent(current));
+            current = parent(current);
+        }
+    }
+};
+
+int main()
+{
+    Heap *myHeap = new Heap();
+    myHeap->insert(99);
+    myHeap->insert(72);
+    myHeap->insert(61);
+    myHeap->insert(58);
+    myHeap->printHeap();
+    myHeap->insert(100);
+    myHeap->printHeap();
+    myHeap->insert(75);
+    myHeap->printHeap();
+    delete myHeap;
+    return 0;
+}
