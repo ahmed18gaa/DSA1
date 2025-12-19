12908b4de0291b09fb84a5712cc37dad87724cc7
ahmed18gaa <ahmed18ga@gmail.com>
2025-12-19 08:54:54 +0300

    Add Binary Search Tree implementation with insert and search functionality

---
+++ DSA_course/rBST/main1.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *left;
+    Node *right;
+    Node(int value)
+    {
+        this->value = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+class BinarySearchTree
+{
+public:
+    Node *root;
+
+public:
+    BinarySearchTree()
+    {
+        root = nullptr;
+    }
+
+    bool insert(int value)
+    {
+        Node *newNode = new Node(value);
+        if (root == nullptr)
+        {
+            root = newNode;
+            return true;
+        }
+        Node *temp = root;
+        while (true)
+        {
+            if (newNode->value == temp->value)
+            {
+                return false;
+            }
+            if (newNode->value < temp->value)
+            {
+                if (temp->left == nullptr)
+                {
+                    temp->left = newNode;
+                    return true;
+                }
+                temp = temp->left;
+            }
+            else
+            {
+                if (temp->right == nullptr)
+                {
+                    temp->right = newNode;
+                    return true;
+                }
+                temp = temp->right;
+            }
+        }
+    }
+
+    bool contains(int value)
+    {
+        Node *temp = root;
+        while (temp)
+        {
+            if (value < temp->value)
+            {
+                temp = temp->left;
+            }
+            else if (value > temp->value)
+            {
+                temp = temp->right;
+            }
+            else
+            {
+                delete (temp);
+                return true;
+            }
+        }
+        delete (temp);
+        return false;
+    }
+
+    bool rContains(Node *currentNode, int value)
+    {
+        if (currentNode == nullptr)
+            return false;
+        if (currentNode->value == value)
+            return true;
+        if (value < currentNode->value)
+        {
+            return rContains(currentNode->left, value);
+        }
+        else
+        {
+            return rContains(currentNode->right, value);
+        }
+    }
+
+    bool rContains(int value)
+    {
+        return rContains(root, value);
+    }
+
+    void destroy(Node *currentNode)
+    {
+        if (currentNode == nullptr)
+            return;
+        if (currentNode->left)
+            destroy(currentNode->left);
+        if (currentNode->right)
+            destroy(currentNode->right);
+        delete currentNode;
+    }
+
+    Node *getRoot()
+    {
+        return root;
+    }
+
+    ~BinarySearchTree() { destroy(root); }
+};
+
+int main()
+{
+    BinarySearchTree *myBST = new BinarySearchTree();
+    myBST->insert(47);
+    myBST->insert(21);
+    myBST->insert(76);
+    myBST->insert(18);
+    myBST->insert(52);
+    myBST->insert(82);
+    myBST->insert(27);
+
+    cout << "\nBST Contains 27:\n";
+    cout << myBST->rContains(27);
+    cout << "\nBST Contains 17:\n";
+    cout << myBST->rContains(17);
+
+    cout << endl;
+    delete myBST;
+    return 0;
+}
